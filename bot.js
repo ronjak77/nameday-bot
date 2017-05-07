cc4ea125c99a22802f282e60e302116a3021b785
Ronja Kääriäinen <Ronja Kriinen>
2017-05-07 17:03:13 +0300

    -

--- bot.js
+++ bot.js
@@ -103,7 +103,7 @@ function receivedMessage(event) {
         }
       }, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-          messageContent = body.name;
+          messageContent = body;
           console.log(body);
         } else {
           console.error("Unable to receive nameday info.");
