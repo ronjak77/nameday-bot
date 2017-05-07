47b2ef033ea5f98efe51ac3dee4335930a5997a8
Ronja Kääriäinen <Ronja Kriinen>
2017-05-07 16:55:35 +0300

    Restructuring

--- bot.js
+++ bot.js
@@ -85,19 +85,29 @@ function receivedMessage(event) {
   var messageText = message.text;
   var messageAttachments = message.attachments;
 
-  if (messageText) {
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the template example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
-        break;
+  var messageContent = "No message";
 
-      default:
-        sendTextMessage(senderID, messageText);
-    }
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
+  if (messageText) {
+
+      request({
+        uri: 'https://nimiapi.herokuapp.com/tomorrow',
+        qs: {
+          api_key: '123456'
+        }
+      }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          messageContent = body.name;
+          console.log(body);
+        } else {
+          console.error("Unable to receive nameday info.");
+          console.error(response);
+          console.error(error);
+        }
+
+
+      });
+
+    sendTextMessage(senderID, messageContent);
   }
 }
 
@@ -122,78 +132,12 @@ function receivedPostback(event) {
 // Sending helpers
 //////////////////////////
 function sendTextMessage(recipientId, messageText) {
-  var messageContent = "No message";
-  request({
-    uri: 'https://nimiapi.herokuapp.com/tomorrow',
-    qs: {
-      api_key: '123456'
-    }
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      messageContent = body;
-      console.log(body);
-    } else {
-      console.error("Unable to receive nameday info.");
-      console.error(response);
-      console.error(error);
-    }
-
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: messageContent
-      }
-    };
-
-    callSendAPI(messageData);
-  });
-
-
-}
-
-function sendGenericMessage(recipientId) {
   var messageData = {
     recipient: {
       id: recipientId
     },
     message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
-      }
+      text: messageText
     }
   };
 
