3dd9f446ba550bbf31569e0607a86ca8a50b3c00
Ronja Kääriäinen <Ronja Kriinen>
2017-05-09 22:21:46 +0300

    Another typo

--- bot.js
+++ bot.js
@@ -194,7 +194,7 @@ function sendNameBasedMessage(name, senderID) {
       var emoticon = emoticons[(Math.floor(Math.random() * emoticons.length))];
       var phrase = jsonBody.name + " viettää nimipäiviään " + jsonBody.resultMsg;
       if(jsonBody.other_names.length >0) {
-        phrase = phrase + " Myös " + other_names.join(', ') + " juhlivat silloin.";
+        phrase = phrase + " Myös " + jsonBody.other_names.join(', ') + " juhlivat silloin.";
       }
 
       messageContent = phrase + emoticon;
