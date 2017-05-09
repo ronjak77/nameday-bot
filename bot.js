f75181169226d918468d17d065298303ab72a36a
Ronja Kääriäinen <Ronja Kriinen>
2017-05-09 17:56:45 +0300

    Now for the real parser

--- bot.js
+++ bot.js
@@ -81,8 +81,8 @@ Date.prototype.addDays = function(days) {
 var finnishDateParser = new chrono.Parser();
 
 // Provide search pattern
-// finnishDateParser.pattern = function () { return /([0-9][.][0-9])+/g }
-finnishDateParser.pattern = function () { return /Christmas/i }
+finnishDateParser.pattern = function () { return /([0-9][.][0-9])+/g }
+// finnishDateParser.pattern = function () { return /Christmas/i }
 
 
 // This function will be called when matched pattern is found
